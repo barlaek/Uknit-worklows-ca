6d2ad32e91b37c05735f68bb7c0acb19ea289efa
WParsec <91538926+WParsec@users.noreply.github.com>
2022-09-13 19:35:46 +0200

    home.mjs
    Created home.mjs and imported nav component there.

--- src/js/components/toggleNav.mjs
+++ src/js/components/toggleNav.mjs
@@ -3,10 +3,13 @@
  * Function will toggle classes to add/remove display: none, and change z-index.
  */
 
-export const toggleBtn = document.querySelector('#toggleBtn');
-export const menu = document.querySelector('.collapse-nav');
 
-toggleBtn.addEventListener('click', () => {
-    menu.classList.toggle('show');
-    menu.classList.toggle('z-10');
-})
+export function toggleNav() {
+    const toggleBtn = document.querySelector('#toggleBtn');
+    const menu = document.querySelector('.collapse-nav');
+
+    toggleBtn.addEventListener('click', () => {
+        menu.classList.toggle('show');
+        menu.classList.toggle('z-10');
+    })
+}
---
+++ src/js/pages/home.mjs
@@ -0,0 +1,4 @@
+import { toggleNav } from "../components/toggleNav.mjs";
+
+// function for collapsing nav
+toggleNav();
