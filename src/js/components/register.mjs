9c827282dbf44858e1305ba16738beb654e59338
RobertDacian <tennerdigitalart@gmail.com>
2022-09-14 12:01:20 -0300

    Finished the post cards on knit's page

--- src/js/components/register.mjs
+++ src/js/components/register.mjs
@@ -4,17 +4,16 @@ export * from "./register.mjs";
 const baseURL = "https://nf-api.onrender.com/";
 
 // DOM variables
-const form = document.querySelector('#signUpForm');
-
+const form = document.querySelector("#signUpForm");
 
 // Function collects data from inputs and puts them in object "values";
 function formSubmit(event) {
-    event.preventDefault();
+  event.preventDefault();
 
-    const data = new FormData(event.target);
-    const values = Object.fromEntries(data.entries());
-    console.log({ values });
+  const data = new FormData(event.target);
+  const values = Object.fromEntries(data.entries());
+  console.log({ values });
 }
 
 // addEventListener on submit of form
-form.addEventListener('submit', formSubmit);
+form.addEventListener("submit", formSubmit);
--- src/js/index.mjs
+++ src/js/index.mjs
@@ -1,4 +1,2 @@
 import * as toggleNav from "./components/toggleNav.mjs";
 import * as register from "./components/register.mjs";
-
-
