70da7d5083f2e908f97863b94f2a3a4cdbfc0b9f
WParsec <91538926+WParsec@users.noreply.github.com>
2022-09-17 13:25:18 +0200

    Search sketch

---
+++ src/js/components/search.mjs
@@ -0,0 +1,30 @@
+const objectArray = [
+    {
+        id: 1,
+        name: 'Amy'
+    },
+    {
+        id: 2,
+        name: 'Tom'
+    },
+    {
+        id: 3,
+        name: 'Robert'
+    },
+];
+
+const input = document.getElementById('input');
+input.addEventListener('keyup', filterSearch);
+
+function filterSearch(event) {
+    const inputValue = event.currentTarget.value.toLowerCase();
+    const resultArray = objectArray.filter((objectArray) => {
+        if (objectArray.name.toLowerCase().startsWith(inputValue)) {
+            return;
+        }
+    });
+    console.log(resultArray);
+
+    // - Empty container that contains the unfiltered products array
+    // - Loop resultArray and build new product for each object in resultArray
+};
