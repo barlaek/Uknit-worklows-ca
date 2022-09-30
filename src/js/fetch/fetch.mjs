00133d2a7fed7584588e0d53e63ec708be92a8bf
RobertDacian <tennerdigitalart@gmail.com>
2022-09-30 10:23:22 -0300

    redoing the file path on some html pages

--- src/js/fetch/fetch.mjs
+++ src/js/fetch/fetch.mjs
@@ -1,47 +1,45 @@
 import { alertWrapper } from "../constants/constants.mjs";
 
-// Async fetch function for login with URL and Body as parameters. 
+// Async fetch function for login with URL and Body as parameters.
 export async function loginFetch(url, body) {
-    try {
-        const response = await fetch(url, body);
-        const data = await response.json();
-        console.log(data);
-
-        // If user is rejected
-        if (!response.ok) {
-            alertWrapper.innerHTML = `<h6>${data.message}</h6>`;
-        }
-        else {
-            // Collect accessToken and send to local storage
-            const accessToken = data.accessToken;
-            localStorage.setItem('accessToken', accessToken);
-            const username = data.name;
-            localStorage.setItem('username', username);
-            window.location.href = "../../../public/home/index.html";
-        }
-    }
-    catch (error) {
-        console.log(error);
-    }
-}
-
-// Async fetch function for register with URL and Body as parameters. 
-export async function registerFetch(url, body) {
+  try {
     const response = await fetch(url, body);
     const data = await response.json();
     console.log(data);
-    if (response.ok) {
-        return data;
+
+    // If user is rejected
+    if (!response.ok) {
+      alertWrapper.innerHTML = `<h6>${data.message}</h6>`;
+    } else {
+      // Collect accessToken and send to local storage
+      const accessToken = data.accessToken;
+      localStorage.setItem("accessToken", accessToken);
+      const username = data.name;
+      localStorage.setItem("username", username);
+      window.location.href = "../../../public/posts/";
     }
-    throw new Error(data.message);
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+// Async fetch function for register with URL and Body as parameters.
+export async function registerFetch(url, body) {
+  const response = await fetch(url, body);
+  const data = await response.json();
+  console.log(data);
+  if (response.ok) {
+    return data;
+  }
+  throw new Error(data.message);
 }
 
 // STANDARD FETCH
 export async function standardFetch(url, body) {
-    const response = await fetch(url, body);
-    const data = await response.json();
-    if (response.ok) {
-        return data;
-    }
-    throw new Error(data.message);
+  const response = await fetch(url, body);
+  const data = await response.json();
+  if (response.ok) {
+    return data;
+  }
+  throw new Error(data.message);
 }
