30027a972a3eafd30c9a5f3df63ec21aa96e5964
WParsec <91538926+WParsec@users.noreply.github.com>
2022-10-05 14:02:15 +0200

    done with delete post

--- src/js/components/editPost.mjs
+++ src/js/components/editPost.mjs
@@ -5,13 +5,3 @@ export function insertValueAsPlaceholder(title, tags, body, media) {
     document.querySelector('input#postMedia').value = media;
 }
 
-export async function editPost(event) {
-    event.preventDefault();
-    const data = new FormData(event.target);
-    const values = Object.fromEntries(data.entries());
-    const accessToken = localStorage.getItem('accessToken');
-    const url = baseURL + submitPostUrl + "/" + id;
-    console.log(values);
-    await standardFetch(url, editProfileBody(values, accessToken));
-    location.reload();
-}
