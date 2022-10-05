08174ee6ae4dba90a84c200c69dc5beee13c718d
RobertDacian <tennerdigitalart@gmail.com>
2022-10-05 16:02:11 -0300

    updated html comment and main navigation on all pages

--- src/js/pages/postSpecs.mjs
+++ src/js/pages/postSpecs.mjs
@@ -1,3 +1,5 @@
+import { toggleNav } from "../components/toggleNav.mjs";
+
 import { baseURL, submitPostUrl } from "../constants/constants.mjs";
 import { postContainer } from "../constants/constants.mjs";
 
@@ -7,76 +9,78 @@ import { deleteHeader, editProfileBody, standardHeader } from "../headers/header
 import { closeModal, modalFunction } from "../components/modals.mjs";
 import { insertValueAsPlaceholder } from "../components/editPost.mjs";
 
+// Running functions
+toggleNav();
+
 const queryString = document.location.search;
 const params = new URLSearchParams(queryString);
-const id = params.get("id")
+const id = params.get("id");
 
-const postSpecUrl = baseURL + "api/v1/social/posts/" + id + "?_author=true&_comments=true&_reactions=true";
-const postTemplate = document.querySelector('#postSpecsTemplate').content;
+const postSpecUrl =
+  baseURL + "api/v1/social/posts/" + id + "?_author=true&_comments=true&_reactions=true";
+const postTemplate = document.querySelector("#postSpecsTemplate").content;
 
 async function createPostSpec() {
-    const accessToken = localStorage.getItem('accessToken');
-    const results = await standardFetch(postSpecUrl, standardHeader(accessToken))
-    const {title, body, tags, media, id} = results;
-    const postClone = document.importNode(postTemplate, true);
-    // Reaction count
-    const reactionCount = () => {
-        if (results.reactions[0]) {
-            return results.reactions[0].count;
-        }
-        else {
-            return 0;
-        }
+  const accessToken = localStorage.getItem("accessToken");
+  const results = await standardFetch(postSpecUrl, standardHeader(accessToken));
+  const { title, body, tags, media, id } = results;
+  const postClone = document.importNode(postTemplate, true);
+  // Reaction count
+  const reactionCount = () => {
+    if (results.reactions[0]) {
+      return results.reactions[0].count;
+    } else {
+      return 0;
     }
-    postClone.querySelector("#postAuthor").innerText = `${results.author.name}`;
-    postClone.querySelector("#postTitle").innerText = `${results.title}`;
-    postClone.querySelector("#postMedia").innerHTML = `<img src="${results.media}">`;
-    postClone.querySelector("#postText").innerHTML = `${results.body}`;
-    postClone.querySelector("#postReactionCount").innerHTML = `${reactionCount()}`;
-    postClone.querySelector("#postAvatar").innerHTML = `<img src="${results.author.avatar}">`;
-    const username = localStorage.getItem("username");
-    if (results.author.name === username) {
-      postClone.querySelector("#editPost").classList.remove("d-none");
-    }
-    if (results.author.name === username) {
-      postClone.querySelector("#followDiv").classList.add("d-none");
-    }
-    if (results.author.name === username) {
-        postClone.querySelector('#deletePost').classList.remove("d-none");
-    }
-    postContainer.appendChild(postClone);
+  };
+  postClone.querySelector("#postAuthor").innerText = `${results.author.name}`;
+  postClone.querySelector("#postTitle").innerText = `${results.title}`;
+  postClone.querySelector("#postMedia").innerHTML = `<img src="${results.media}">`;
+  postClone.querySelector("#postText").innerHTML = `${results.body}`;
+  postClone.querySelector("#postReactionCount").innerHTML = `${reactionCount()}`;
+  postClone.querySelector("#postAvatar").innerHTML = `<img src="${results.author.avatar}">`;
+  const username = localStorage.getItem("username");
+  if (results.author.name === username) {
+    postClone.querySelector("#editPost").classList.remove("d-none");
+  }
+  if (results.author.name === username) {
+    postClone.querySelector("#followDiv").classList.add("d-none");
+  }
+  if (results.author.name === username) {
+    postClone.querySelector("#deletePost").classList.remove("d-none");
+  }
+  postContainer.appendChild(postClone);
 
-    const editButton = document.querySelector("#editPost");
-    editButton.addEventListener('click', () => {
-        insertValueAsPlaceholder(title, tags, body, media);
-        modalFunction();
-    })
+  const editButton = document.querySelector("#editPost");
+  editButton.addEventListener("click", () => {
+    insertValueAsPlaceholder(title, tags, body, media);
+    modalFunction();
+  });
 
-    document.querySelector('#deletePost').addEventListener('click', () => {
-        deletePost();
-    })
-    
+  document.querySelector("#deletePost").addEventListener("click", () => {
+    deletePost();
+  });
 }
 createPostSpec();
 
-document.querySelector('#editPostForm').addEventListener('submit', (event) => {
-    editPost(event);
-})
+document.querySelector("#editPostForm").addEventListener("submit", (event) => {
+  editPost(event);
+});
 
 async function editPost(event) {
-    event.preventDefault();
-    const data = new FormData(event.target);
-    const values = Object.fromEntries(data.entries());
-    const accessToken = localStorage.getItem('accessToken');
-    const url = baseURL + submitPostUrl + "/" + id;
-    console.log(values);
-    await standardFetch(url, editProfileBody(values, accessToken));
-    location.reload();
+  event.preventDefault();
+  const data = new FormData(event.target);
+  const values = Object.fromEntries(data.entries());
+  const accessToken = localStorage.getItem("accessToken");
+  const url = baseURL + submitPostUrl + "/" + id;
+  console.log(values);
+  await standardFetch(url, editProfileBody(values, accessToken));
+  location.reload();
 }
 
 async function deletePost() {
-    const url = baseURL + submitPostUrl + "/" + id;
-    const accessToken = localStorage.getItem('accessToken');
-    await standardFetch(url, deleteHeader(accessToken))
-    location.href = "../../public/home";
+  const url = baseURL + submitPostUrl + "/" + id;
+  const accessToken = localStorage.getItem("accessToken");
+  await standardFetch(url, deleteHeader(accessToken));
+  location.href = "../../public/home";
 }
